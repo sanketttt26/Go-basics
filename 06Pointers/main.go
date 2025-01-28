fd2703118b730f31d7e411a6c640e779cc9c239a
Pawar <pawar.sanket@siemens.com>
2025-01-28 12:16:29 +0530

    Learn pointers

---
+++ 06Pointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Learning Pointers")
+	var a int = 10
+	var p *int // declaring a pointer variable
+
+	p = &a // assigning the address of 'a' to the pointer 'p'
+
+	fmt.Println("Value of a:", a)
+	fmt.Println("Address of a:", &a)
+	fmt.Println("Value of p (address of a):", p)
+	fmt.Println("Value at address stored in p:", *p)
+
+	*p = 20 // changing the value at the address stored in 'p'
+	fmt.Println("New value of a:", a)
+
+}
